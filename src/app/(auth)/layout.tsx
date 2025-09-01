f0bb71b357408f52b4a72aad8ac2df4ea394e707
Ramesh Kumar <rameskum.ms@outlook.com>
2025-09-01 10:55:48 +0530

    feat: add logo and update AuthLayout with logo link

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,9 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import Logo from "@/../public/logo.svg";
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
     return (
         <div className="relative flex min-h-svh flex-col items-center justify-center">
-            <div className="flex w-full max-w-sm flex-col gap-6">{children}</div>
+            <div className="flex w-full max-w-sm flex-col gap-6">
+                <Link className="flex items-center gap-2 self-center font-medium" href="/">
+                    <Image src={Logo} alt="Logo" width={32} height={32} />
+                    RDL UI
+                </Link>
+                {children}
+            </div>
         </div>
     );
 }
